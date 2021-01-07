31c8ef040c2042de713e12ae3f9dd851147296fc
BenRamsden30 <Ben-Ramsden@hotmail.com>
2021-01-07 13:37:22 +0000

    Added basic getting data from the APi's I think but to be tested, TODO Add in error handling.

--- RestockingMicroService/Proxies/SupplierRealProxy.cs
+++ RestockingMicroService/Proxies/SupplierRealProxy.cs
@@ -3,6 +3,7 @@ using RestockingMicroService.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace RestockingMicroService.Proxies
@@ -21,8 +22,30 @@ namespace RestockingMicroService.Proxies
             return await _context.Suppliers.FirstOrDefaultAsync(a => a.SupplierID == Id);
         }
 
-        public Task<List<Products>> GetSupplierProducts(int Id)
+        public async Task<List<Products>> GetSupplierProducts(int Id)
         {
+            var Sup = await _context.Suppliers.FirstOrDefaultAsync(s => s.SupplierID == Id);
+            string Address = Sup.Webaddress;
+
+            //Builds the location to be aimed for
+            HttpClient client = new HttpClient();
+            var RequestBuilder = new UriBuilder(Address);
+            RequestBuilder.Path = "/Api/Product";
+            String url = RequestBuilder.ToString();
+
+            //Checks to see if the deisred location was reached
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsAsync<List<Products>>();
+            }
+            else
+            {
+
+            }
+
             throw new NotImplementedException();
         }
 
