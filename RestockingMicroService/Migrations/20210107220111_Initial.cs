894df857199211c8128af80ae108ebbf6f070a04
BenRamsden30 <Ben-Ramsden@hotmail.com>
2021-01-07 22:05:01 +0000

    DB migrations done oath still commented out

---
+++ RestockingMicroService/Migrations/20210107220111_Initial.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestockingMicroService.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Restocks",
+                columns: table => new
+                {
+                    RestockId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountName = table.Column<string>(nullable: true),
+                    CardNumber = table.Column<string>(maxLength: 16, nullable: true),
+                    ProductID = table.Column<int>(nullable: false),
+                    Gty = table.Column<int>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    ProductName = table.Column<string>(nullable: true),
+                    ProductEan = table.Column<string>(nullable: true),
+                    TotalPrice = table.Column<double>(nullable: false),
+                    SupplierID = table.Column<int>(nullable: false),
+                    Approved = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restocks", x => x.RestockId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Suppliers",
+                columns: table => new
+                {
+                    SupplierID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SupplierName = table.Column<string>(nullable: true),
+                    Webaddress = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suppliers", x => x.SupplierID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restocks");
+
+            migrationBuilder.DropTable(
+                name: "Suppliers");
+        }
+    }
+}
